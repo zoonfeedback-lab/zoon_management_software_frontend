e354dcbfdccfbac927dd4759b5a462724b9f7e77
rehmanghani2 <135846061+rehmanghani2@users.noreply.github.com>
2026-05-08 15:07:55 +0500

    feat{Intersnhip-Applications}: The Internship tab from sidebar is implemented in admin

---
+++ app/(dashboard)/internship-applications/page.tsx
@@ -0,0 +1,9 @@
+import InternshipApplicationsClient from "./page.client";
+
+export const metadata = {
+  title: "Internship Applications | ZOON Admin Portal",
+};
+
+export default function InternshipApplicationsPage() {
+  return <InternshipApplicationsClient />;
+}
